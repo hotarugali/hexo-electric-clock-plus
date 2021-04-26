78f2cc54f387949a585da983e48178149e82a003
hotarugali <hotarugali@163.com>
2021-04-26 13:53:24 +0800

    2021年 04月 26日 星期一 13:53:24 CST

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function common_injector(name, item_comfig, temple_html_text, js_text, css_text)
 hexo.extend.filter.register('after_generate', function () {
     if (hexo.config.electric_clock.enable) {
         var electric_clock = hexo.config.electric_clock;
-        var css_text = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/hotarugali/hexo-electric-clock-plus/ccs/clock.css">`;
+        var css_text = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/hotarugali/hexo-electric-clock-plus/css/clock.css">`;
         var js_text = `<script type="text/javascript">
             loc = $.ajax({
                     url: "https://ipapi.co/json/",  //json文件位置，文件名
